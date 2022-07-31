65fbc32eccbe122523ceb89b52bc585a6d9ace6b
Samuel <samuelsoutosilva14@gmail.com>
2022-07-31 17:52:33 -0300

    feat(exceptions): add HttpException

---
+++ src/exceptions/http.exeption.ts
@@ -0,0 +1,15 @@
+class HttpException extends Error {
+  statusCode: number;
+  
+  message: string;
+  
+  constructor(status: number, message: string) {
+    super(message);
+    this.statusCode = status;
+    this.message = message;
+  }
+}
+   
+export default HttpException;
+
+// new HttpException(404, 'Post not found')
