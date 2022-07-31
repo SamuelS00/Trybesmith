7af8c0fad528523605e78a8dcf208d228b83d690
Samuel <samuelsoutosilva14@gmail.com>
2022-07-31 17:58:40 -0300

    feat(app): add route /products and errorMiddleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import morgan from 'morgan';
+import errorMiddleware from './middlewares/error.handler';
+import productRouter from './routes/product.route';
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan('tiny'));
+
+app.use('/products', productRouter);
+app.use(errorMiddleware);
 
 export default app;
