f25c1b4317e7ab8f0d91a8adf6581aa78a02e27c
Samuel <samuelsoutosilva14@gmail.com>
2022-08-01 16:14:19 -0300

    feat(app): add user router to /users route

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import errorMiddleware from './middlewares/error.handler';
 import productRouter from './routes/product.route';
+import userRouter from './routes/user.route';
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(morgan('tiny'));
 
 app.use('/products', productRouter);
+app.use('/users', userRouter);
 app.use(errorMiddleware);
 
 export default app;
