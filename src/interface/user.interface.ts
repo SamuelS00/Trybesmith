ad046a08eef89587eb5b6e5e689681262f7a5e67
Samuel <samuelsoutosilva14@gmail.com>
2022-08-01 16:11:40 -0300

    fix: the name of the interface import and export

--- src/interface/user.interface.ts
+++ src/interface/user.interface.ts
@@ -1,4 +1,4 @@
-interface User {
+interface IUser {
   id?: number;
   username: string;
   classe: string;
@@ -6,4 +6,4 @@ interface User {
   password: string;
 }
 
-export default User;
+export default IUser;
