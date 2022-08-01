1b78210227a92914ab71ffa981dbc11cde123c4e
Samuel <samuelsoutosilva14@gmail.com>
2022-08-01 16:12:53 -0300

    feat(contro/user): add getById and create methods

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import HttpStatus from 'http-status-codes';
+import UserService from '../services/user.service';
+
+class UserController {
+  constructor(private userService = new UserService()) { }
+
+  public getById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    const user = await this.userService.getById(+id);
+    res.status(HttpStatus.ACCEPTED).json(user);
+  };
+
+  public create = async (req: Request, res: Response) => {
+    const user = { ...req.body };
+
+    const token = await this.userService.create(user);
+    res.status(HttpStatus.CREATED).json(token);
+  };
+}
+
+export default UserController;
