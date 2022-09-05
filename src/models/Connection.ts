96599c1866150bbd54865b94064c9984d2864a5a
Samuel <samuelsoutosilva14@gmail.com>
2022-09-05 16:02:36 -0300

    feat: add Connection

---
+++ src/models/Connection.ts
@@ -0,0 +1,30 @@
+import { OkPacket, RowDataPacket, ResultSetHeader, FieldPacket } from 'mysql2';
+import dotenv from 'dotenv';
+import mysql, { Pool } from 'mysql2/promise';
+import IConnection from './IConnection';
+
+dotenv.config();
+
+class Connection implements IConnection {
+  private static connection: Pool;
+
+  constructor() {
+    Connection.connection = mysql.createPool({
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+    });
+  }
+
+  public execute = async <
+  T extends RowDataPacket[][] | RowDataPacket[] | OkPacket | OkPacket[] | ResultSetHeader>(
+    sql: string,
+    values?: any | any[] | { [param: string]: any },
+  ): Promise<[T, FieldPacket[]]> => {
+    const result = await Connection.connection.execute(sql, values);
+
+    return result as [T, FieldPacket[]];
+  };
+}
+
+export default new Connection();
