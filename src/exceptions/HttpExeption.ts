cc085990298d23e926a4a2f197505d9821e53b74
Samuel <samuelsoutosilva14@gmail.com>
2022-09-05 16:04:38 -0300

    feat: add http exception

---
+++ src/exceptions/HttpExeption.ts
@@ -0,0 +1,13 @@
+class HttpException extends Error {
+  statusCode: number;
+  
+  message: string;
+  
+  constructor(status: number, message: string) {
+    super(message);
+    this.statusCode = status;
+    this.message = message;
+  }
+}
+
+export default HttpException;
