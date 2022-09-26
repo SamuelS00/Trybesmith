a6ca9d2f7038bd3fdcfcf783f4d29e7d04697aea
Samuel <samuelsoutosilva14@gmail.com>
2022-09-25 21:53:09 -0300

    refactor: factories as class

--- src/factories/LoginUserFactory.ts
+++ src/factories/LoginUserFactory.ts
@@ -1,15 +1,13 @@
 import LoginUserController from '../controllers/LoginUserController';
 import LoginUserService from '../services/LoginUserService';
-import JwtToken from '../services/JwtService';
 import MySqlUserRepository from '../models/repositories/implementations/MySqlUsersRepository';
 
-const makeLoginUserController = () => {
-  const model = new MySqlUserRepository();
-  const jwtToken = new JwtToken();
-  const service = new LoginUserService(model, jwtToken);
-  const controller = new LoginUserController(service);
+export default class LoginUserControllerFactory {
+  static make() {
+    const model = new MySqlUserRepository();
+    const service = new LoginUserService(model);
+    const controller = new LoginUserController(service);
 
-  return controller;
-};
-
-export default makeLoginUserController();
+    return controller;
+  }
+}
