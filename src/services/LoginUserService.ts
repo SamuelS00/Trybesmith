e8c56267f9d1961b0215145d6c8497067d0a593d
Samuel <samuelsoutosilva14@gmail.com>
2022-09-25 21:44:36 -0300

    refactor: remove jwtService repository

--- src/services/LoginUserService.ts
+++ src/services/LoginUserService.ts
@@ -1,5 +1,5 @@
 import { CredentialsInvalid } from '../errors';
-import JwtToken from './JwtService';
+import JwtToken from '../helpers/JwtService';
 import { UserCredentials } from '../interfaces/IUser';
 import { IUserRepository } from '../models/repositories/IUsersRepository';
 import validateCredentials from '../validations/credentialsValidations';
@@ -7,16 +7,14 @@ import validateCredentials from '../validations/credentialsValidations';
 export default class LoginUserService {
   constructor(
     private userRepository: IUserRepository,
-    private tokenService: JwtToken,
   ) { }
 
   execute = async (credentials: UserCredentials): Promise<string> => {
     validateCredentials(credentials);
     const [user] = await this.userRepository.getUserByUser(credentials);
-
     if (!user || credentials.password !== user.password) throw new CredentialsInvalid();
 
-    const token = this.tokenService.generate({ id: user.id });
+    const token = JwtToken.generate({ id: user.id });
     return token;
   };
 }
