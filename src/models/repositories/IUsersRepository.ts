5f85795f6e1fe356b2be855d849f81561b1e742d
Samuel <samuelsoutosilva14@gmail.com>
2022-09-25 21:40:06 -0300

    feat: add IUser repository interface

--- src/models/repositories/IUsersRepository.ts
+++ src/models/repositories/IUsersRepository.ts
@@ -3,5 +3,5 @@ import { IUser, UserCredentials, UserDTO } from '../../interfaces/IUser';
 export interface IUserRepository {
   getUserById(id: number): Promise<UserDTO[]>;
   getUserByUser({ username, password }: UserCredentials): Promise<UserDTO[]>
-  createUser({ username, classe, level, password }: IUser): Promise<UserDTO>;
+  create({ username, classe, level, password }: IUser): Promise<UserDTO>;
 }
