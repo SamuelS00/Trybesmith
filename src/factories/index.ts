79ff57f558d170d78fab9ceb1d2d87e4b23f7749
Samuel <samuelsoutosilva14@gmail.com>
2022-09-25 21:52:41 -0300

    feat: add index in order to export

---
+++ src/factories/index.ts
@@ -0,0 +1,15 @@
+import CreateOrderControllerFactory from './CreateOrderFactory';
+import CreateProductControllerFactory from './CreateProductFactory';
+import CreateUserControllerFactory from './CreateUserFactory';
+import GetAllOrderControllerFactory from './GetAllOrderFactory';
+import GetAllProductControllerFactory from './GetAllProductFactory';
+import LoginUserControllerFactory from './LoginUserFactory';
+
+export {
+  CreateOrderControllerFactory,
+  CreateProductControllerFactory,
+  CreateUserControllerFactory,
+  GetAllOrderControllerFactory,
+  GetAllProductControllerFactory,
+  LoginUserControllerFactory,
+};
