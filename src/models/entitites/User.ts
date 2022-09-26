8d2198c01d8379460fd07a4011f69e3fc838e3af
Samuel <samuelsoutosilva14@gmail.com>
2022-09-25 21:38:57 -0300

    feat(model): add User entities

--- src/models/entitites/User.ts
+++ src/models/entitites/User.ts
@@ -1,14 +1,24 @@
 import Entity from './Entity';
 import { IUser } from '../../interfaces/IUser';
 
+import Name from './validators/Name';
+import InvalidParam from '../../errors/InvalidParam';
+import Level from './validators/Level';
+
 export default class User extends Entity<IUser> {
   private constructor(props: IUser) {
     super(props);
   }
 
-  static create = (props: IUser) => {
-    const user = new User(props);
+  static create(props: IUser) {
+    const username: Name | InvalidParam = Name.create(props.username);
+    const classe: Name | InvalidParam = Name.create(props.classe);
+    const level: Level | InvalidParam = Level.create(props.level);
 
-    return user;
-  };
+    if (username instanceof InvalidParam) return new InvalidParam('username');
+    if (classe instanceof InvalidParam) return new InvalidParam('class');
+    if (level instanceof InvalidParam) return new InvalidParam('level');
+
+    return new User(props);
+  }
 }
