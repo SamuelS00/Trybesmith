5fa1aff8eb477f1c77bed103a0d7ac266c8ecb6c
Samuel <samuelsoutosilva14@gmail.com>
2022-09-25 21:47:42 -0300

    refactor: add validation with entities

--- src/services/CreateProductService.ts
+++ src/services/CreateProductService.ts
@@ -1,15 +1,18 @@
+import InvalidParam from '../errors/InvalidParam';
 import { IProduct } from '../interfaces/IProduct';
+import Product from '../models/entitites/Product';
 import { IProductsRepository } from '../models/repositories/IProductsRepository';
-import validateProduct from '../validations/productValidations';
 
 export default class CreateProductService {
   constructor(
-    private productsRepository: IProductsRepository,
+    private repository: IProductsRepository,
   ) { }
 
-  execute = async ({ name, amount }: IProduct) => {
-    validateProduct({ name, amount });
-    const product = await this.productsRepository.create({ name, amount });
-    return product;
-  };
+  async execute({ name, amount }: IProduct) {
+    const productOrError = Product.create({ name, amount });
+    if (productOrError instanceof InvalidParam) throw productOrError;
+  
+    const newProduct = await this.repository.create(productOrError.props);
+    return newProduct;
+  }
 }
