3e8524e8c16d54a56935192c3823caccdde533bd
Samuel <samuelsoutosilva14@gmail.com>
2022-09-25 21:38:44 -0300

    feat(model): add Product entities

--- src/models/entitites/Product.ts
+++ src/models/entitites/Product.ts
@@ -1,14 +1,21 @@
 import Entity from './Entity';
 import { IProduct } from '../../interfaces/IProduct';
 
+import Name from './validators/Name';
+import InvalidParam from '../../errors/InvalidParam';
+
 export default class Product extends Entity<IProduct> {
   private constructor(props: IProduct) {
     super(props);
   }
 
-  static create = (props: IProduct) => {
-    const product = new Product(props);
+  static create(props: IProduct) {
+    const amount: Name | InvalidParam = Name.create(props.amount);
+    const name: Name | InvalidParam = Name.create(props.name);
 
-    return product;
-  };
+    if (amount instanceof InvalidParam) return new InvalidParam('amount');
+    if (name instanceof InvalidParam) return new InvalidParam('name');
+
+    return new Product(props);
+  }
 }
