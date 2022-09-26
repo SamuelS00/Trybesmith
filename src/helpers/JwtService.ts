69aaa1663748019719f34a3fb0246e9c258995ff
Samuel <samuelsoutosilva14@gmail.com>
2022-09-25 21:43:19 -0300

    feat: add JwtService

---
+++ src/helpers/JwtService.ts
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+import { JwtPayload } from '../types/JwtPayload';
+
+export default class JwtToken {
+  public static generate = ({ id }: JwtPayload) => {
+    const token = jwt.sign({ id }, 'secret', { expiresIn: '20m' });
+    return token;
+  };
+
+  public static decode = async (token: string): Promise<number> => {
+    const { id } = jwt.verify(token, 'secret') as JwtPayload;
+
+    return id;
+  };
+}
