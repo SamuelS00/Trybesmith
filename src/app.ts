ff18dd6acc06b753eea0f4be9df27123e62d4d00
Samuel <samuelsoutosilva14@gmail.com>
2022-11-05 19:19:05 -0300

    feat: add swagger in the application

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './docs/swagger_output.json';
 
 import errorMiddleware from './middlewares/errorHandler';
 
@@ -10,16 +12,19 @@ import {
   userRouter,
 } from './routes';
 
+/** Middleware */
 const app = express();
-
 app.use(express.json());
 app.use(morgan('tiny'));
 
+/** Swagger */
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use(errorMiddleware);
+
+/** Routes */
 app.use(authRouter);
 app.use(productRouter);
 app.use(userRouter);
 app.use(orderRouter);
-
-app.use(errorMiddleware);
 
 export default app;
